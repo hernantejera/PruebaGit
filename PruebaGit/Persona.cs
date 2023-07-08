ffd88966f84a5ad36c9a767dcda23d372d64b9ef
hernantejera <hernanmtejera@gmail.com>
2023-07-08 11:25:47 -0300

    creada clase Persona

---
+++ PruebaGit/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGit
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set;}
+
+        public string Saludar()
+        {
+            return $"Hola minombre es {Nombre} y mi apellido es {Apellido}";
+        }  
+    }
+}
